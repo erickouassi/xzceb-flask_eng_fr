0471addc05b1271a39bf39d2b3742d6fc6358b87
erickouassi <hello@erickouassi.com>
2021-12-05 17:02:24 -0500

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -11,19 +11,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ["apikey"]
-url = os.environ["url"]
+apikey = os.environ['apikey']
+url = os.environ['url']
 
 ## Example for the Dallas location
 # from ibm_watson import LanguageTranslatorV3
 # from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 # Authentication
-authenticator = IAMAuthenticator("-YZ8yTFvzdBEhSNR1Yw6O2XYibUgjLKJTP8kRQpv5lf6")
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version="2018-05-01", authenticator=authenticator
 )
 
-language_translator.set_service_url("https://api.us-south.language-translator.watson.cloud.ibm.com/instances/99f4129b-9e31-42e7-bc71-8c48c24a25bf")
+language_translator.set_service_url(url)
 
 # e2f_translator_function
 def english_to_french(english_text):
